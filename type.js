8701f7b2c67de21dbb62b31effc66ed0351ba872
mdnasiruddin78 <nasiruddinanik54@gmail.com>
2024-08-29 01:31:24 +0600

    first commit

---
+++ type.js
@@ -0,0 +1,22 @@
+var name = "hello world";
+console.log(name);
+// string
+var country = "bangladesh";
+console.log(country);
+
+
+// boolean
+var passed = false;
+console.log(passed);
+
+var rich = true;
+console.log(rich);
+
+var poor = true;
+console.log(typeof poor);
+
+var country = "india";
+console.log(typeof country);
+
+var price = 500;
+console.log(typeof price);
---
+++ variable.js
@@ -0,0 +1,16 @@
+var weight = 38;
+var price = 1995;
+var money = 2005;
+
+var age;
+console.log(weight);
+var age = 67;
+console.log(age)
+var height = 85;
+console.log(height);
+var size = 546;
+console.log(size)
+
+const car = "red car";
+console.log(car);
+
